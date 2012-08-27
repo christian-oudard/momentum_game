bfe4d630fc811c3285b45c0a3079f3c87243856c
Christian Oudard <christian.oudard@gmail.com>
2012-08-27 16:03:28 -0700

    Refactoring. Made a simple widget system to separate concerns in the display manager.

--- displaymanager.py
+++ displaymanager.py
@@ -1,10 +1,4 @@
-# TODO
-# make field rotatable
-# make screen_size, screen_origin, pixels_per_unit, fullscreen, etc
-#   settable through functions
-
-import pygame
-from pygame.locals import *
+import pygame as p
 
 from singletonmixin import Singleton
 from vector import Coord, Point2dFloat
@@ -12,14 +6,14 @@ from environment import Environment
 from inputmanager import InputManager
 
 # debug switches #
-_SHOW_JOYSTICK = True
-_SHOW_FPS = True
+SHOW_JOYSTICK = True
+SHOW_FPS = True
 
 # constants #
-_FULL = False
+FULLSCREEN = False
 
 # colors #
-_BG_COLOR = (128,128,255)
+BG_COLOR = (128,128,255)
 
 # handles #
 ENV = Environment.getInstance()
@@ -32,37 +26,32 @@ class DisplayManager(Singleton):
     def init(self, screen_size):
         self.screen_size = Coord(screen_size)
         self.pixels_per_unit = 20
-        flags = HWSURFACE | DOUBLEBUF
-        if _FULL: flags |= FULLSCREEN
-        self.screen = pygame.display.set_mode(tuple(self.screen_size), flags)
+        flags = p.HWSURFACE | p.DOUBLEBUF
+        if FULLSCREEN:
+            flags |= p.FULLSCREEN
+        self.screen = p.display.set_mode(tuple(self.screen_size), flags)
 
         self.screen_origin = self.screen_size / 2
         
-        if _SHOW_JOYSTICK:
-            self.__joystick_surface = pygame.Surface((30,30), SRCALPHA, 32)
-        if _SHOW_FPS:
-            pygame.font.init()
-            self.__font = pygame.font.SysFont(pygame.font.get_default_font(), 20)
-            self.__fps = 0.0
-            self.__fps_surface = None
-            self._debug_update_fps_surface()
+        self.widgets = []
+
+        if SHOW_JOYSTICK:
+            self.widgets.append(JoystickWidget(self))
+        if SHOW_FPS:
+            self.fps = 0.0
+            self.widgets.append(FPSWidget(self))
 
     def draw(self):
-        self.screen.fill(_BG_COLOR)
+        self.screen.fill(BG_COLOR)
 
         ## possibly split into terrain then object draw order
-
         for obj in ENV.obj_list:
             obj.graphic.draw(obj)
-            
-        if _SHOW_JOYSTICK:
-            self._debug_update_joystick_surface()
-            self.screen.blit(self.__joystick_surface, (10, self.screen_size.y - self.__joystick_surface.get_height() - 10))
-        if _SHOW_FPS:
-            self._debug_update_fps_surface()
-            self.screen.blit(self.__fps_surface, (self.screen_size.x - self.__fps_surface.get_width() - 10, 10))
-        
-        pygame.display.flip()
+
+        for widget in self.widgets:
+            widget.draw(self.screen)
+
+        p.display.flip()
 
     def to_screen(self, pos):
         """Convert pos to screen coordinates.
@@ -74,21 +63,41 @@ class DisplayManager(Singleton):
             pos = Point2dFloat(pos)
         return self.screen_origin + pos * self.pixels_per_unit
 
-    if _SHOW_JOYSTICK:
-        def _debug_update_joystick_surface(self):
-            self.__joystick_surface.lock()
-            self.__joystick_surface.fill((0,0,0,0))
-            pygame.draw.line(self.__joystick_surface, (128,255,128), (14-10,14), (14+10,14), 2)
-            pygame.draw.line(self.__joystick_surface, (128,255,128), (14,14-10), (14,15+10), 2)
-            joypos = Coord(INPUT.x_axis, -INPUT.y_axis) * 8
-            pygame.draw.circle(self.__joystick_surface, (196,64,64), tuple(joypos + (15,15)), 4)
-            self.__joystick_surface.unlock()
-    
-    if _SHOW_FPS:
-        def debug_set_fps(self, fps):
-            self.__fps = fps
-            self._debug_update_fps_surface()
-        def _debug_update_fps_surface(self):
-            self.__fps_surface = self.__font.render('%.0f fps' % self.__fps, True, (255,255,255))
-            
+    def set_fps(self, fps):
+        self.fps = fps
 
+
+class FPSWidget(object):
+    def __init__(self, display):
+        self.display = display
+        self.font = p.font.SysFont(p.font.get_default_font(), 20)
+        self.color = (255,255,255)
+
+    def draw(self, screen):
+        surface = self.font.render(
+            '%.0f fps' % self.display.fps,
+            True,
+            self.color,
+        )
+        x = self.display.screen_size.x - surface.get_width() - 10
+        y = 10
+        screen.blit(surface, (x, y))
+
+            
+class JoystickWidget(object):
+    def __init__(self, display):
+        self.display = display
+        self.surface = p.Surface((30,30), p.SRCALPHA, 32)
+
+    def draw(self, screen):
+        self.surface.lock()
+        self.surface.fill((0,0,0,0))
+        p.draw.line(self.surface, (128,255,128), (14-10,14), (14+10,14), 2)
+        p.draw.line(self.surface, (128,255,128), (14,14-10), (14,15+10), 2)
+        joypos = Coord(INPUT.x_axis, -INPUT.y_axis) * 8
+        p.draw.circle(self.surface, (196,64,64), tuple(joypos + (15,15)), 4)
+        self.surface.unlock()
+
+        x = 10
+        y = self.display.screen_size.y - self.surface.get_height() - 10
+        screen.blit(self.surface, (x, y))
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from __future__ import division
-import pygame
-from pygame import event
-from pygame.locals import *
+import pygame as p
 from singletonmixin import SingletonException
 from displaymanager import DisplayManager
 from environment import Environment
@@ -30,20 +28,21 @@ INPUT = InputManager.getInstance()
 def main():
 
     # init #
-    pygame.init()
+    p.init()
+    p.font.init()
     ENV.init()
     DISP.init(SCREENSIZE)
     INPUT.init()
 
     if SHOW_FPS:
-        UPDATE_FPS = USEREVENT
-        pygame.time.set_timer(UPDATE_FPS, 700)
-    1000
-    ##TEMP
+        update_fps = p.USEREVENT + 1
+        p.time.set_timer(update_fps, 700)
+
+    ##TEMP, set up level
     g = Graphic()
     wg = WallGraphic()
     
-    player = Particle(pos=(2,0),velocity=(0,0), mass=.5, graphic=g)
+    player = Particle(pos=(2,2),velocity=(0,0), mass=.5, graphic=g)
     ENV.add_obj(player)
     
     ENV.add_obj(Particle(pos=(0,3),velocity=(-1,3), mass=1, graphic=g))
@@ -60,35 +59,35 @@ def main():
     
     # vars #
     done = False
-    clock = pygame.time.Clock()
+    clock = p.time.Clock()
     clock.tick()  # necessary, or the first tick will be very large
     
     # program loop #
     while not done:
         # events #
-        for e in event.get():
-            if e.type == QUIT: done = True
-            elif e.type == KEYDOWN:
+        for e in p.event.get():
+            if e.type == p.QUIT: done = True
+            elif e.type == p.KEYDOWN:
                 key = e.key
-                if key == K_ESCAPE:
-                    event.post(event.Event(QUIT))
-                elif key == K_UP:
+                if key == p.K_ESCAPE:
+                    p.event.post(p.event.Event(p.QUIT))
+                elif key == p.K_UP:
                     INPUT.up_last = True
-                elif key == K_DOWN:
+                elif key == p.K_DOWN:
                     INPUT.up_last = False
-                elif key == K_RIGHT:
+                elif key == p.K_RIGHT:
                     INPUT.right_last = True
-                elif key == K_LEFT:
+                elif key == p.K_LEFT:
                     INPUT.right_last = False
-            elif e.type == MOUSEMOTION:
+            elif e.type == p.MOUSEMOTION:
                 pass
-            elif e.type == MOUSEBUTTONDOWN:
+            elif e.type == p.MOUSEBUTTONDOWN:
                 pass
-            elif e.type == MOUSEBUTTONUP:
+            elif e.type == p.MOUSEBUTTONUP:
                 pass
             elif SHOW_FPS:
-                if e.type == UPDATE_FPS:
-                    DISP.debug_set_fps(clock.get_fps())
+                if e.type == update_fps:
+                    DISP.set_fps(clock.get_fps())
 
         # logic #
         INPUT.update()
@@ -104,7 +103,7 @@ def main():
         
     # end program loop #
 
-    pygame.quit()
+    p.quit()
     
 if __name__ == '__main__':
     main()
