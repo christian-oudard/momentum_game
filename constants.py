8c51c11cfce684e10392e90fe2f8374d444327aa
Christian Oudard <christian.oudard@gmail.com>
2012-08-28 16:09:50 -0700

    Tweaking physics constants.

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import math
 # Physics constants.
 
 # Speed limits.
-min_speed = 0.1
+min_speed = 0.2
 min_speed2 = min_speed ** 2
 max_speed = 40.0
 max_speed2 = max_speed ** 2
@@ -18,10 +18,10 @@ drag_curve = [
 ]
 
 # Player physics.
-player_strength = 40.0
+player_strength = 30.0
 player_turn_rate = 1.0 # full rotations / second
 player_turn_rate_radians = player_turn_rate * (2 * math.pi)
 
 # Collision restitution.
-restitution_wall = 0.6
+restitution_wall = 0.4
 restitution_particle = 1.1
